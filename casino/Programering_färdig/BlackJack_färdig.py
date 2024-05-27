f550fab85d55450854a42f167b7174ce52b4fe85
Ebbe Johansson <ebbe.johansson1@gmail.com>
2024-05-27 22:01:55 +0200

    gjort en del av vad joel sade men vet inte

--- casino/Programering_färdig/BlackJack_färdig.py
+++ casino/Programering_färdig/BlackJack_färdig.py
@@ -15,27 +15,27 @@ import random
 
 class Beting():
     def __init__(self) -> None:
-        self.worth = 100
-        self.beting = int(input("How much do dare to bet: "))
+        self.worth: int = 100
+        self.beting: int = int(input("You currently have: ", self.worth, "\NHow much do dare to bet: (enter number between 1 and )", self.worth))
 
 class Deck():
     def __init__(self)-> None:
-#        self.deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-#                    2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A", "J", "Q", "K", "A",  "J", "Q", "K", "A",  "J", "Q", "K", "A", ]
+        self.deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                    2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A", "J", "Q", "K", "A",  "J", "Q", "K", "A",  "J", "Q", "K", "A", ]
         self.deck = []
         self.create_deck()
 
-    def create_deck(self):
-        self.deck = []
-        for i in range(1, 14):
+#    def create_deck(self):
+#        self.deck = []
+#        for i in range(1, 14):
             # spade - unicode
-            self.cards.append(Card('\u2660', i))
+#            self.cards.append(Card('\u2660', i))
             # heart - unicode
-            self.cards.append(Card('\u2665', i))
+#            self.cards.append(Card('\u2665', i))
             # diamond - unicode
-            self.cards.append(Card('\u2666', i))
+#            self.cards.append(Card('\u2666', i))
             # club - unicode
-            self.cards.append(Card('\u2663', i))
+#            self.cards.append(Card('\u2663', i))
 
 
 
@@ -65,10 +65,10 @@ class Hand():
 
     def dealCard(self):
         for _ in range(2):
-            self.card = random.choice(deck)
+            self.card: int = random.choice(deck)
             self.turn.append(self.card)
         
-    def RemoveValueFromeDeck():
+    def RemoveValueFromeDeck() -> None:
         for i in range(len(self.player.turn)):
             self.deck.deck.remove(self.player.turn[i])
         
@@ -103,12 +103,12 @@ class Play():
     def __init__(self) -> None:
         self.player_in = True
         self.dealer_in = True
-        self.stayOrHit = 0
+        self.stayOrHit: int = 0
         self.player = Hand("Ada")
         self.computer = Hand("Computer")
         self.deck = Deck()
 
-    def game(self):
+    def game(self) -> None:
         self.player.calculate_total()
         self.player.dealCard()
         
@@ -117,7 +117,7 @@ class Play():
             print(F"Dealer has {self.computer.hand} and x")
             print(f"You have {self.player.hand} for a total of {self.player.total}")
         if self.player_in:
-            self.stayOrHit = input("1: Stay\n2: Hit\n")
+            self.stayOrHit = int(input("1: Stay\n2: Hit\n"))
             if self.stayOrHit.isdigit():
                 int(self.stayOrHit)
             else:
@@ -162,10 +162,10 @@ class Play():
             self.worth -= self.beting
 
 
-    def main():
+    def main() -> None:
         your_cards = self.dealCards
 
-    def giveCards(self):
+    def giveCards(self) -> None:
         for _ in range(2):
             self.dealCard(self.dealerHand)
             self.dealCard(self.playerHand)
